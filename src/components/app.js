03ea42beac6fbd5aa3592be5855409f4a131bed6
Vallabha Rayudu <rayudu.ikkurthi@gmail.com>
2018-09-07 18:07:42 +0530

    Added bootstrap 3 and changes Navigation UI
    Added bootstrap 3 and changes Navigation UI

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
 
+
 export default class App extends Component {
+componentDidMount(){
+
+}
+
   render() {
     return (
-      <div>
-      	<h3>Route List</h3>
-      	<ul>
-          <li><a href="/firstSample">firstSample</a></li>
-          <li><a href="/formSample">formSample</a></li>
-          <li><a href="/ajaxSample">ajaxSample</a></li>
-          <li><a href="/parentToChild">parentToChild</a></li>
-          <li><a href="/childToParent">childToParent</a></li>
-          <li><a href="/objectUpdate">objectInlineUpdate</a></li>
-          <li><a href="/reduxSample">Redux Sample Without Action</a></li>
-          <li><a href="/ReduxWithAction">Redux With Action</a></li>
-       	</ul>
-      </div>
+      <nav className="navbar navbar-default navbar-static-top">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                    <li><a href="/firstSample">firstSample</a></li>
+                    <li><a href="/formSample">formSample</a></li>
+                    <li><a href="/ajaxSample">ajaxSample</a></li>
+                    <li><a href="/parentToChild">parentToChild</a></li>
+                    <li><a href="/childToParent">childToParent</a></li>
+                    <li><a href="/objectUpdate">objectInlineUpdate</a></li>
+                    <li className="dropdown">
+                        <a id="dLabel" href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        Redux 
+                        <span className="caret"></span></a>
+                        <ul className="dropdown-menu" aria-labelledby="dLabel">
+                            <li><a href="/reduxSample">Redux Sample Without Action</a></li>
+                            <li><a href="/ReduxWithAction">Redux With Action</a></li>
+                        </ul>
+                    </li>
+              </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -8,19 +8,27 @@ import Routers from './components/router';
 //import JsonLearn from './components/jsonLearn';
 import reducers from './reducers';
 import ReduxPromise from 'redux-promise';
+import jquery from 'jquery';
+window.$ = window.jQuery=jquery;
+require ('bootstrap');
+
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <div className="row">
-      <div className="col-md-3 col-lg-3 col-sm-4 col-xs-12">
+  <div>
+      <div>
           <App />
       </div>
-      <div className="col-md-9 col-lg-9 col-sm-8 col-xs-12">
-          <Routers/>
+
+      <div className="row">
+        
+        <div className="col-md-9 col-lg-9 col-sm-8 col-xs-12">
+            <Routers/>
+        </div>
+        <div className="clearfix"></div>
       </div>
-      <div className="clearfix"></div>
     </div>
   </Provider>
   , document.querySelector('.container-fluid'));
