240d38ad07b9a85e12f49a74c5b9dc7fb2e1c3b5
Vallabha Rayudu <rayudu.ikkurthi@gmail.com>
2018-09-07 18:33:29 +0530

    routes adjusted

--- src/components/app.js
+++ src/components/app.js
@@ -15,14 +15,22 @@ componentDidMount(){
                     <li><a href="/firstSample">firstSample</a></li>
                     <li><a href="/formSample">formSample</a></li>
                     <li><a href="/ajaxSample">ajaxSample</a></li>
-                    <li><a href="/parentToChild">parentToChild</a></li>
-                    <li><a href="/childToParent">childToParent</a></li>
+                    <li className="dropdown">
+                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        Parent Child Relations 
+                        <span className="caret"></span></a>
+                        <ul className="dropdown-menu">
+                            <li><a href="/parentToChild">parentToChild</a></li>
+                            <li><a href="/childToParent">childToParent</a></li>
+                        </ul>
+                    </li>
+
                     <li><a href="/objectUpdate">objectInlineUpdate</a></li>
                     <li className="dropdown">
-                        <a id="dLabel" href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                         Redux 
                         <span className="caret"></span></a>
-                        <ul className="dropdown-menu" aria-labelledby="dLabel">
+                        <ul className="dropdown-menu">
                             <li><a href="/reduxSample">Redux Sample Without Action</a></li>
                             <li><a href="/ReduxWithAction">Redux With Action</a></li>
                         </ul>
